1b43f5aaaa62bf0dd46f8a78b282ac3472d2d367
Loto-Kenneth <116004412+Loto-Kenneth@users.noreply.github.com>
2026-07-09 22:46:53 +0800

    chore: removed vercel oicd in server env and added babel plugin for react compiler

--- env/server.ts
+++ env/server.ts
@@ -3,7 +3,6 @@ import * as v from "valibot";
 
 export const serverEnv = createEnv({
   server: {
-    VERCEL_OIDC_TOKEN: v.pipe(v.string(), v.minLength(1)),
     SENTRY_AUTH_TOKEN: v.pipe(v.string(), v.minLength(1)),
     NODE_ENV: v.optional(
       v.picklist(["development", "test", "production"]),
